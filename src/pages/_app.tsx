f43610cc388ebb351959f28bdfbff55ee092b824
@tienpham1 <tienpham@pixelcent.com>
2022-09-10 10:10:57 +0700

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import "assets/scss/now-ui-kit.scss?v=1.4.0";
 import "assets/demo/demo.css?v=1.4.0";
 import "assets/demo/react-demo.css?v=1.4.0";
 import "assets/demo/nucleo-icons-page-styles.css?v=1.4.0";
+import './index.scss';
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
---
+++ src/pages/about.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const About: NextPage = () => {
+  return (
+    <>
+      <h1>HEllo</h1>
+    </>
+  );
+};
