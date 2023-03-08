045e61f006ede55ae69d0a13e0f2ea66ea3978e3
@khoiopixelcentgit <khoidinh@pixelcent.com>
2023-03-08 08:07:15 +0700

    update

--- src/components/CardItemList/index.tsx
+++ src/components/CardItemList/index.tsx
@@ -72,103 +72,107 @@ const ListServices = memo(
   }: Props) => {
     const { user } = useAuth();
     return (
-      <>
-        <Grid
-          xs={3}
-          key={id}
-          sx={{
-            display: "flex",
-            minHeight: "200px",
-            minWidth: "640px",
-            paddingBottom: "24px",
-            cursor: "pointer",
-          }}
-          className={clsx(
-            { [classes.stopWorking]: isTemporarilyStopWorking || isDelete },
-            classes.row,
-            className
-          )}
-        >
-          <Grid className={classes.boxImg}>
-            <img src={src} alt="anh"></img>
-          </Grid>
-          <Grid
-            sx={{
-              padding: "24px 14px 14px 14px",
-              flexGrow: "1",
-              justifyContent: "space-between",
-              flexShrink: "1",
-              borderTopRightRadius: "10px",
-              borderBottomRightRadius: "10px",
-              backgroundColor: "var(--white-color)",
-              boxShadow: "var(--bui-shadow-100)",
-            }}
-          >
-            <Grid className={classes.boxLocation}>
-              <FontAwesomeIcon icon={faSignsPost}></FontAwesomeIcon>
-              <div>Ganh Day Commue Phu Quoc Vinwonder</div>
-            </Grid>
-            <Grid className={classes.boxTitle}>
-              <p>{title}</p>
-            </Grid>
-            {isHotel && (
-              <Grid
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  paddingTop: "8px",
-                }}
-              >
-                <Grid
-                  sx={{
-                    padding: "6px 14px",
-                    backgroundColor: "var(--primary-light-color)",
-                    color: "var(--white-color)",
-                    borderRadius: "10px",
-                    marginRight: "16px",
-                  }}
-                >
-                  Hotel
-                </Grid>
-                <Stars numberOfStars={rate} />
-              </Grid>
-            )}
-            <Grid className={classes.boxReview} sx={{ paddingTop: "8px" }}>
-              {rate !== 0 && (
-                <Grid className={classes.boxReview}>
-                  <FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon>
-                  <span>
-                    {rate}{" "}
-                    <span className={classes.numberOfReviews}>
-                      ( {numberOfReviewers} reviews)
-                    </span>
-                  </span>
-                </Grid>
-              )}
-            </Grid>
+      <Grid className={classes.linkView}>
+        <Link href={`/${linkView}/:${id}`}>
+          <a>
             <Grid
+              xs={3}
+              key={id}
               sx={{
                 display: "flex",
-                justifyContent: "space-between",
-                flexDirection: "column",
+                minHeight: "200px",
+                minWidth: "640px",
+                paddingBottom: "24px",
+                cursor: "pointer",
               }}
+              className={clsx(
+                { [classes.stopWorking]: isTemporarilyStopWorking || isDelete },
+                classes.row,
+                className
+              )}
             >
-              <Grid className={classes.boxPrice}>
-                {discount !== 0 && (
-                  <span>
-                    {fCurrency2((price * (100 - discount)) / 100)} VND
-                  </span>
-                )}
-                <p>{fCurrency2(price)} VND</p>
+              <Grid className={classes.boxImg}>
+                <img src={src} alt="anh"></img>
               </Grid>
-              <Grid className={classes.boxViewMore}>
-                <p>View more</p>
-                <FontAwesomeIcon icon={faAnglesRight}></FontAwesomeIcon>
+              <Grid
+                sx={{
+                  padding: "24px 14px 14px 14px",
+                  flexGrow: "1",
+                  justifyContent: "space-between",
+                  flexShrink: "1",
+                  borderTopRightRadius: "10px",
+                  borderBottomRightRadius: "10px",
+                  backgroundColor: "var(--white-color)",
+                  boxShadow: "var(--bui-shadow-100)",
+                }}
+              >
+                <Grid className={classes.boxLocation}>
+                  <FontAwesomeIcon icon={faSignsPost}></FontAwesomeIcon>
+                  <div>Ganh Day Commue Phu Quoc Vinwonder</div>
+                </Grid>
+                <Grid className={classes.boxTitle}>
+                  <p>{title}</p>
+                </Grid>
+                {isHotel && (
+                  <Grid
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      paddingTop: "8px",
+                    }}
+                  >
+                    <Grid
+                      sx={{
+                        padding: "6px 14px",
+                        backgroundColor: "var(--primary-light-color)",
+                        color: "var(--white-color)",
+                        borderRadius: "10px",
+                        marginRight: "16px",
+                      }}
+                    >
+                      Hotel
+                    </Grid>
+                    <Stars numberOfStars={rate} />
+                  </Grid>
+                )}
+                <Grid className={classes.boxReview} sx={{ paddingTop: "8px" }}>
+                  {rate !== 0 && (
+                    <Grid className={classes.boxReview}>
+                      <FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon>
+                      <span>
+                        {rate}{" "}
+                        <span className={classes.numberOfReviews}>
+                          ( {numberOfReviewers} reviews)
+                        </span>
+                      </span>
+                    </Grid>
+                  )}
+                </Grid>
+                <Grid
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    flexDirection: "column",
+                  }}
+                >
+                  <Grid className={classes.boxPrice}>
+                    {discount !== 0 && (
+                      <span>
+                        {fCurrency2((price * (100 - discount)) / 100)} VND
+                      </span>
+                    )}
+                    <p>{fCurrency2(price)} VND</p>
+                  </Grid>
+                  <Grid className={classes.boxViewMore}>
+                    <p>View more</p>
+                    <FontAwesomeIcon icon={faAnglesRight}></FontAwesomeIcon>
+                  </Grid>
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
-        </Grid>
-      </>
+          </a>
+        </Link>
+      </Grid>
     );
   }
 );
